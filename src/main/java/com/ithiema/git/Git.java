564035d019ace68cd6a7bd0316b438babdf337a5
ZJW <487142518@qq.com>
2022-07-29 19:31:18 +0800

    hot-fix commit1

--- src/main/java/com/ithiema/git/Git.java
+++ src/main/java/com/ithiema/git/Git.java
@@ -7,5 +7,6 @@ public class Git {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("hello git5!");
+        System.out.println("hello world");
     }
 }
