5b2f25360a771469b584c26174cb7838ec7d33bc
ZJW <487142518@qq.com>
2022-07-29 19:21:20 +0800

    second commit

--- src/main/java/com/ithiema/git/Git.java
+++ src/main/java/com/ithiema/git/Git.java
@@ -3,5 +3,7 @@ package com.ithiema.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
